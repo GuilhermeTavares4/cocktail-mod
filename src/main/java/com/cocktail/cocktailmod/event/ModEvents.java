75b7cd5c7e4a7a1875c4426d8bd3eca0d5f84857
juanmadeira <21133654+juanmadeira@users.noreply.github.com>
2026-06-14 01:12:02 -0300

    refactor(morph): add Attributes import

--- src/main/java/com/cocktail/cocktailmod/event/ModEvents.java
+++ src/main/java/com/cocktail/cocktailmod/event/ModEvents.java
@@ -2,6 +2,7 @@ package com.cocktail.cocktailmod.event;
 
 import com.cocktail.cocktailmod.CocktailMod;
 import com.cocktail.cocktailmod.effect.MorphEffects;
+import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.entity.player.Player;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
@@ -13,10 +14,9 @@ public class ModEvents {
     @SubscribeEvent
     public static void onMobEffectExpired(MobEffectEvent.Expired event) {
         if (!(event.getEntity() instanceof Player player)) return;
-        if (event.getEffectInstance().toString().contains("morph")) { SwapPackets.sendSwapRequest(); }
 
-        if (event.getEffectInstance().getEffect() == MorphEffects.ZOMBIE_MORPH ) {
-            player.getAttribute(net.minecraft.world.entity.ai.attributes.Attributes.MOVEMENT_SPEED).setBaseValue(0.1);
-        }
+        var effect = event.getEffectInstance().getEffect();
+        if (effect.toString().contains("morph")) { SwapPackets.sendSwapRequest(); } // remove morph
+        if (effect == MorphEffects.ZOMBIE_MORPH ) { player.getAttribute(Attributes.MOVEMENT_SPEED).setBaseValue(0.1); }
     }
 }
